556b22f1a66d851e4d4b722efef935bf7a74db58
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-03-06 21:15:02 -0600

    define initialization in teacher model

--- lib/models/teacher.py
+++ lib/models/teacher.py
@@ -1,2 +1,12 @@
 # lib/models/teacher.py
 from models.__init__ import CONN, CURSOR
+
+class Teacher:
+
+    # Dictionary of objects saved to the database.
+    all = {}
+
+    def __init__(self, name, classes, id=None):
+        self.name = name
+        self.classes = classes
+        self.id = id
