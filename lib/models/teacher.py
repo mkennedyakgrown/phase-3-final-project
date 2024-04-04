09bf78f5472ac8e235bfcc81bcab950e5972ca56
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-04-03 19:58:16 -0500

    Update delete_row to remove Teacher association from class_name objects

--- lib/models/teacher.py
+++ lib/models/teacher.py
@@ -83,13 +83,6 @@ class Teacher:
         CURSOR.execute(sql, (self.id,))
         CONN.commit()
 
-        sql = """
-            DELETE FROM teacher_class_names
-            WHERE teacher_id = ?
-        """
-        CURSOR.execute(sql, (self.id,))
-        CONN.commit()
-
         del type(self).all[self.id]
 
         self.id = None
